e593da265427fd30c8be1e04f17d42b6314fdf87
Mikhail <eremkinma@Mail.ru>
2022-07-12 22:15:38 +0300

    0

--- News_portal/NewsPaper/news/filters.py
+++
@@ -1,9 +0,0 @@
-from django_filters import FilterSet
-from .models import Post
-
-
-class PostFilter(FilterSet):
-    class Meta:
-        model = Post
-        fields = {'creation_time': ['year__gt'], 'header': ['icontains'], }
-        # 'author': ['icontains']
--- News_portal/NewsPaper/news/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm
-from .models import Post
-
-
-class PostForm(ModelForm):
-    class Meta:
-        model = Post
-        fields = ['header', 'a_or_n_text', 'category', 'author']
